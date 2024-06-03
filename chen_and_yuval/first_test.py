5935504908039c7e00a54cd19bc64597efe94a85
Chen Tasker <chentasker1@gmail.com>
2024-06-03 13:53:25 +0300

    added basic net

--- chen_and_yuval/first_test.py
+++ chen_and_yuval/first_test.py
@@ -5,3 +5,58 @@ from matplotlib import pyplot as plt
 
 from tv_opt_layers.layers.general_tv_2d_layer import GeneralTV2DLayer
 
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+
+def unet(input_size=(128, 128, 1)):
+    inputs = layers.Input(input_size)
+
+    # Encoder
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(inputs)
+    conv1 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool1)
+    conv2 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(pool2)
+    conv3 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(pool3)
+    conv4 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv4)
+    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(pool4)
+    conv5 = layers.Conv2D(1024, 3, activation='relu', padding='same')(conv5)
+
+    # Decoder
+    up6 = layers.Conv2D(512, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv5))
+    merge6 = layers.concatenate([conv4, up6], axis=3)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(merge6)
+    conv6 = layers.Conv2D(512, 3, activation='relu', padding='same')(conv6)
+
+    up7 = layers.Conv2D(256, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv6))
+    merge7 = layers.concatenate([conv3, up7], axis=3)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(merge7)
+    conv7 = layers.Conv2D(256, 3, activation='relu', padding='same')(conv7)
+
+    up8 = layers.Conv2D(128, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv7))
+    merge8 = layers.concatenate([conv2, up8], axis=3)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(merge8)
+    conv8 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv8)
+
+    up9 = layers.Conv2D(64, 2, activation='relu', padding='same')(layers.UpSampling2D(size=(2, 2))(conv8))
+    merge9 = layers.concatenate([conv1, up9], axis=3)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(merge9)
+    conv9 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv9)
+
+    conv10 = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    model = models.Model(inputs, conv10)
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
